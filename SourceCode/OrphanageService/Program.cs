d7aac2ff2b5d47949774adf932793ab8cfc7b335
Mohamad Hesham Alkhateb <heshamnet16@hotmail.com>
2018-04-13 09:54:38 +0200

    #49 reset the OrphanageService to windows service application

--- SourceCode/OrphanageService/Program.cs
+++ SourceCode/OrphanageService/Program.cs
@@ -24,20 +24,20 @@ namespace OrphanageService
                     PowerShellExecuter.CreateAndRunPsFileScript();
 
                 // commit to debug the service
-                //ServiceBase[] ServicesToRun;
-                //ServicesToRun = new ServiceBase[]
-                //{
-                //        new SelfHostServiceBase()
-                //};
-                //ServiceBase.Run(ServicesToRun);
+                ServiceBase[] ServicesToRun;
+                ServicesToRun = new ServiceBase[]
+                {
+                        new SelfHostServiceBase()
+                };
+                ServiceBase.Run(ServicesToRun);
 
                 //uncommit to debug the service
-                string baseUrl = Properties.Settings.Default.BaseURI;
-                WebApp.Start<Startup>(baseUrl);
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("Orphan Service is started on port 1515");
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.ReadLine();
+                //string baseUrl = Properties.Settings.Default.BaseURI;
+                //WebApp.Start<Startup>(baseUrl);
+                //Console.ForegroundColor = ConsoleColor.Green;
+                //Console.WriteLine("Orphan Service is started on port 1515");
+                //Console.ForegroundColor = ConsoleColor.White;
+                //Console.ReadLine();
             }
             catch (Exception exc)
             {
